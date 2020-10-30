1af4b9eadb6e4b046b8aabb893a163628a83a0db
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-10-29 23:13:23 -0300

    navigavigation between pages

---
+++ gatsbyjs_blog/src/components/Profile/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Profile = ()  => (
+    <div className="Profile-wrapper">
+        <h1>Paula Pompeo</h1>
+        <h2>Full Stack Developer</h2>
+
+        <p>I am a civil engineer who turned into a full stack developer and I am passionate about learning new techs</p>
+    </div>
+)
+
+export default Profile;
