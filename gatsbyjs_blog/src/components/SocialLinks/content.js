77f6d1ef276b16fe61a13f89556ac13e24ac0272
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-11-01 16:12:15 -0300

    add social links

---
+++ gatsbyjs_blog/src/components/SocialLinks/content.js
@@ -0,0 +1,26 @@
+// const handle = "pompeopaula";
+
+const links = [
+    {
+        label: "Github",
+        url: 'https://github.com/paulapompeo'
+    },
+    {
+        label: "Instagram",
+        url: 'https://www.instagram.com/paulapompeo_'
+    },
+    {
+        label: "Twitter",
+        url: 'https://twitter.com/pompeopaula'
+    },
+    {
+        label: "Youtube",
+        url: 'https://www.youtube.com/channel/UCMKkxjjoPDY8KtoyZomA5qQ?view_as=subscriber'
+    },
+    {
+        label: "Unsplash",
+        url: 'https://github.com/paulapompeo'
+    },
+]
+
+export default links;
