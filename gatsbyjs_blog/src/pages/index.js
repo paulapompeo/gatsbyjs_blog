1e657c663f3572876864654b77b406b8e05d81e0
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-11-03 17:45:25 -0300

    md e url

--- gatsbyjs_blog/src/pages/index.js
+++ gatsbyjs_blog/src/pages/index.js
@@ -1,24 +1,61 @@
 import React from "react";
-import { Link } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from '../components/PostItem'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <PostItem 
-      slug="/about/"
-      background="red"
-      category="Misc"
-      date="01 de novembro de 2020"
-      timeToRead="5"
-      title="Diga nao ao Medium: tenha sua propria plataforma"
-      description="Algumas razoes para voce ter sua propria plataforma ao inves de solucoes como o Medium"
-    />
-   
-  </Layout>
-)
+const IndexPage = () => {
+
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
+            wordCount {
+              words
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const postList = allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postList.map(({
+        node: {
+          frontmatter: {background, category, date, description, title },
+          timeToRead,
+          fields: {slug}, 
+        },
+      }) => (
+        <PostItem 
+          slug={slug}
+          background={background}
+          category={category}
+          date={date}
+          timeToRead={timeToRead}
+          title={title}
+          description={description}
+        />
+      ))}
+    </Layout>
+  )
+}
 
 export default IndexPage
