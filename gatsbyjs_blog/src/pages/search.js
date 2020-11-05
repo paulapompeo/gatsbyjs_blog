70b1ad9bdbea1d0c58ea54858b73538908fd9846
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-11-05 18:10:44 -0300

    create search page

---
+++ gatsbyjs_blog/src/pages/search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import Search from '../components/Search'
+
+const searchPage = () => (
+    <Layout>
+        <SEO title="Search" />
+        <Search />
+    </Layout>
+)
+
+export default searchPage;
