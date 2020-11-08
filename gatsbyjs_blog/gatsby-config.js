ce7af5f9516c0e9e7c063082ca3ea988ae46c37e
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-11-08 11:03:56 -0300

    create manifest

--- gatsbyjs_blog/gatsby-config.js
+++ gatsbyjs_blog/gatsby-config.js
@@ -83,15 +83,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Paula Pompeo`,
+        short_name: `Paula Pompeo`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
